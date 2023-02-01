9b2d3c0763f3be5acfbdf070685f435dd2c0fd97
shaikanwar68252 <124122341+shaikanwar68252@users.noreply.github.com>
2023-02-01 18:23:55 +0530

    Create sdgsd.c

---
+++ sdgsd.c
@@ -0,0 +1,30 @@
+QUESTION-19:- Decimal to program 
+
+#include<stdio.h>
+#include<math.h>
+int convert(int dec);
+int main()
+{
+	int dec;
+	printf("enter a decimal number: ");
+	scanf("%d", &dec);
+	printf("the octal number of %d is %d", dec, convert(dec));
+	return 0;
+}
+
+int convert(int dec)
+{
+	int octal=0, i=1;
+	while(dec!=0)
+	{
+		octal=0, i=1;
+		dec/=8;
+		i*=10;
+	}
+	return octal;
+}
+
+OUTPUT:-
+
+enter a decimal number: 0.12
+the octal number of 0 is 0
