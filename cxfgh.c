8b1713b34d42232556b4c591b6e8d8b8baeed4c9
shaikanwar68252 <124122341+shaikanwar68252@users.noreply.github.com>
2023-02-01 18:21:58 +0530

    Create cxfgh.c

---
+++ cxfgh.c
@@ -0,0 +1,16 @@
+QUESTION-17:-
+
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("enter the decimal value: ");
+    scanf("%d",&n);
+    printf("the hex value is: %x",n);
+    return 0;
+}
+
+OUTPUT:-
+
+enter the decimal value: 0.12
+the hex value is: 0
