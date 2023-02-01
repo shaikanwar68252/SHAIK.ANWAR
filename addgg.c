8d0fa4edcfc017c3876ea475e14301d7a0de6f97
shaikanwar68252 <124122341+shaikanwar68252@users.noreply.github.com>
2023-02-01 18:23:11 +0530

    Create addgg.c

---
+++ addgg.c
@@ -0,0 +1,16 @@
+QUESTION-18:-
+
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter the hex value:");
+	scanf("%x",&n);
+	printf("the decimal value is : %d",n);
+	return 0;
+}
+
+OUTPUT:-
+
+enter the hex value:12
+the decimal value is : 18
