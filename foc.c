a8a85bc2731ceb5e243aec5ed24497f51f2a6767
shaikanwar68252 <124122341+shaikanwar68252@users.noreply.github.com>
2023-02-01 17:31:25 +0530

    Create foc.c

---
+++ foc.c
@@ -0,0 +1,30 @@
+QUESTION - 1:-Generation of number series 1,2,3,....n
+input:-
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,i;
+	printf("enter the limit of a: ");
+	scanf("%d",&a);
+	printf("\n");
+	for(i=1;i<=a;i++)
+	{
+		if(i==a)
+		printf("%d",i);
+		else
+		printf("%d",i);
+	}
+	getch();
+	return 0;
+}
+
+
+
+
+
+OUTPUT:-
+	enter the limit of a: 10
+
+12345678910
